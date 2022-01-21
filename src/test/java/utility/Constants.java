44fd1b2ddee2e7d57633cbbf6d6e1b133a265e51
dalia945 <97290648+dalia945@users.noreply.github.com>
2022-01-21 11:01:36 +0530

    Update Constants.java

--- src/test/java/utility/Constants.java
+++ src/test/java/utility/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     public static final String Path_TestData = ".\\testData\\";
     public static final String File_TestData = "SwagData.xlsx";
     public static final String File_TestData2 = "RegistrationData.xlsx";
-    public static final String Configprop_filepath = "C:\\Users\\admin\\eclipse-workspace\\com.org.lnt.Finance\\TestData\\config1.properties";
+    public static final String Configprop_filepath = "C:\\Users\\Dell\\git\\SwagExercise\\testData\\config1.properties";
     public static final String Log4j_path = ".\\Log4file\\log4j.properties";
 }
